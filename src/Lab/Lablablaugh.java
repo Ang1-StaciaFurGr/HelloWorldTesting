0eca98d63b15bbbb7c1ce3deb139c61cadacd946
user <user@MF04>
2019-02-21 14:46:40 +0800

    Minecraft to World

--- src/Lab/Lablablaugh.java
+++ src/Lab/Lablablaugh.java
@@ -4,7 +4,7 @@ public class Lablablaugh {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Hello Minecraft~~!!");
+		System.out.print("Hello World~~!!");
 	}
 
 }
